7fcbe777d290cf36be605d96aa4c1985621900b9
molpopogen <krthornt@uci.edu>
2021-05-26 13:10:43 -0700

    write sims to output dir

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
+if [ ! -d output ]
+then
+    mkdir output
+fi
+
 for p in 0.0 0.1 0.25 0.5
 do
     SEED=$RANDOM
-    ./target/release/overlapping_generations -N 500 -n 10000 -P $p -S $SEED -x 1e-3 \
-        -t treefile_$p --nthreads 12 --nreps 500
+    ./target/release/overlapping_generations -N 500 -n 20000 -P $p -S $SEED -x 1e-3 \
+        -t output/treefile_$p --nthreads 12 --nreps 500
 done
