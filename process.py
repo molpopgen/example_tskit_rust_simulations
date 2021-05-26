5b3382aa1938a755421460cd48aefa2e58983ed2
molpopogen <krthornt@uci.edu>
2021-05-26 11:18:49 -0700

    fix process.py

--- process.py
+++ process.py
@@ -7,9 +7,5 @@ print("p div")
 for treefile in glob.glob("*.trees"):
     ts = tskit.load(treefile)
     params = eval(ts.provenance(0).record)
-    samples = np.where(ts.tables.nodes.flags == 1)
-    div = ts.diversity([samples[0]], mode="branch")
-    mroots = 0
-    for t in ts.trees():
-        mroots = max(mroots, t.num_roots)
-    print(params["psurvival"], div[0], mroots)
+    div = ts.diversity([[i for i in ts.samples()]], mode="branch")
+    print(params["psurvival"], div[0])
