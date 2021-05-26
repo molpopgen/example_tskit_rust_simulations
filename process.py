6863147b3ee2b5a124abd63b89057b31d220f096
molpopgen <krthornt@uci.edu>
2021-05-26 10:40:33 -0700

    update process.py

--- process.py
+++ process.py
@@ -1,16 +1,15 @@
-import tskit
-import numpy as np
 import glob
 
+import numpy as np
+import tskit
+
 print("p div")
-for treefile in glob.glob('*.trees'):
+for treefile in glob.glob("*.trees"):
     ts = tskit.load(treefile)
-    s = treefile.split('_')
-    psurvival = float(s[1])
+    params = eval(ts.provenance(0).record)
     samples = np.where(ts.tables.nodes.flags == 1)
     div = ts.diversity([samples[0]], mode="branch")
     mroots = 0
     for t in ts.trees():
         mroots = max(mroots, t.num_roots)
-    print(psurvival, div[0], mroots)
-
+    print(params["psurvival"], div[0], mroots)
