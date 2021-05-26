37a918f4b5b499367053dc0fa6cb5559f3ab58b5
molpopogen <krthornt@uci.edu>
2021-05-26 11:19:12 -0700

    run lots of reps!!

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ for p in 0.0 0.1 0.25 0.5
 do
     SEED=$RANDOM
     ./target/release/overlapping_generations -N 500 -n 10000 -P $p -S $SEED -x 1e-3 \
-        -t treefile_$p --nthreads 4 --nreps 500
+        -t treefile_$p --nthreads 12 --nreps 500
 done
