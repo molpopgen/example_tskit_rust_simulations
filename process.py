ea32a84a1214a46e7c94978894bc762dce9007ca
molpopgen <krthornt@uci.edu>
2021-05-27 11:55:21 -0700

    fix process.py

--- process.py
+++ process.py
@@ -4,7 +4,7 @@ import numpy as np
 import tskit
 
 print("p div")
-for treefile in glob.glob("*.trees"):
+for treefile in glob.glob("output/*.trees"):
     ts = tskit.load(treefile)
     params = eval(ts.provenance(0).record)
     div = ts.diversity([[i for i in ts.samples()]], mode="branch")
