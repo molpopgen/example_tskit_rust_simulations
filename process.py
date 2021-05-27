8d23412d6971600e31830cc90b9bf7303a057c9d
molpopgen <krthornt@uci.edu>
2021-05-27 15:05:53 -0700

    script fixes

--- process.py
+++ process.py
@@ -3,9 +3,9 @@ import glob
 import numpy as np
 import tskit
 
-print("p recrate div")
+print("N p recrate div")
 for treefile in glob.glob("output/*.trees"):
     ts = tskit.load(treefile)
     params = eval(ts.provenance(0).record)
     div = ts.diversity([[i for i in ts.samples()]], mode="branch")
-    print(params["psurvival"], params["recrate"], div[0])
+    print(params["N"], params["psurvival"], params["recrate"], div[0])
--- summarise.R
+++ summarise.R
@@ -1,6 +1,6 @@
 library(dplyr)
 x = read.table("stats", header=TRUE)
 
-means = x %>% group_by(p, recrate) %>% summarise(mean_pi = mean(div), var_pi = var(div), cv_pi = sd(div)/mean(div))
+means = x %>% group_by(N, p, recrate) %>% summarise(mean_pi = mean(div), var_pi = var(div), cv_pi = sd(div)/mean(div), .groups = "drop")
 
 print(means)
